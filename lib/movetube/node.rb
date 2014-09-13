8c879f114dd4242397f5d6d6578e74baaeb5b7d2
Andrea Leopardi <an.leopardi@gmail.com>
2014-09-13 16:50:05 +0200

    Add a megaton of documentation

--- lib/movetube/node.rb
+++ lib/movetube/node.rb
@@ -1,12 +1,25 @@
 require 'movetube/parser'
 
+# A generic node is an episode or a subtitle which points to a file on the disk.
 class Movetube::Node
+  # @!attribute [r] show
+  #   @return the show name
+  # @!attribute [r] season
+  #   @return the season number
+  # @!attribute [r] episode
+  #   @return the episode number
+  # @!attribute [r] extension
+  #   @return the extension of the source file
   attr_reader :show, :season, :episode, :extension
 
+  # All the recognized extensions, both for video files and subtitles.
   VALID_EXTENSIONS = Movetube::VIDEO_EXTENSIONS + Movetube::SUBTITLE_EXTENSIONS
 
-  def initialize(filename)
-    @filename = filename
+  # Create a new `Node` from a given `path`.
+  # @param [String] path The path that points to the node's source file.
+  def initialize(path)
+    @path = path
+    @filename = File.basename(path)
     data = Movetube::Parser.parse(@filename)
 
     @show = data[:show]
@@ -15,20 +28,26 @@ class Movetube::Node
     @extension = data[:extension]
   end
 
+  # Whether the file is a valid episode or subtitle.
+  # @return [Boolean] `true` if the filename is parsable and its extension is
+  #   valid, `false` otherwise.
   def valid?
     Movetube::Parser.parsable?(@filename) && valid_extension?
   end
 
+  # Whether this node is an episode.
   def episode?
     valid? && Movetube::VIDEO_EXTENSIONS.include?(@extension)
   end
 
+  # Whether this node is a subtitle.
   def subtitle?
     valid? && Movetube::SUBTITLE_EXTENSIONS.include?(@extension)
   end
 
   private
 
+  # Whether the extension of this node is a recognized extension.
   def valid_extension?
     VALID_EXTENSIONS.include?(@extension)
   end
--- lib/movetube/parser.rb
+++ lib/movetube/parser.rb
@@ -1,4 +1,7 @@
+# A parser to extract metadata from a filename.
 class Movetube::Parser
+  # The regular expression that extracts the season and the episode from a
+  # filename.
   SEASON_EPISODE_REGEX = /
   (
     s(?<season>\d{1,3}) e(?<episode>\d{1,3})
@@ -7,14 +10,31 @@ class Movetube::Parser
   )
   /xi
 
+  # Return a hash of metadata given a filename.
+  # @param [String] filename A filename of an episode or a subtitle. Note that
+  #   the filename isn't the complete path, but just the *basename*.
+  # @return [Hash] An hash with keys `:show`, `:season`, `:episode` and
+  #  `:extension`.
   def self.parse(filename)
     self.new.parse(filename)
   end
 
+  # Whether the given filename is parsable as an episode/subtitle or not.
+  # @param [String] filename The filename (which is the *basename*, not the full
+  #   path).
+  # @return [Boolean]
   def self.parsable?(filename)
     !self.new.parse(filename).has_value?(nil)
   end
 
+  # This method is here so that `Parser.parse` can create a new `Parser`
+  # instance and call this method on it. It's not meant to be used publicly.
+  #
+  # @param [String] filename The filename (which is the *basename*, not the full
+  #   path).
+  # @return [Hash]
+  # @private
+  # @see Movetube::Parser.parse
   def parse(filename)
     @filename = clean_filename(filename)
 
@@ -28,29 +48,46 @@ class Movetube::Parser
 
   private
 
+  # Convert each occurrence of `"_"` in `"."` in the given `filename`.
+  # @param [String] filename
+  # @return [String]
   def clean_filename(filename)
     filename.gsub('_', '.').chomp
   end
 
+  # Extract the show name from `@filename`.
+  # @return [String] if a match is found.
+  # @return [nil] if no match is found.
   def extract_show
-    @filename
+    show = @filename
       .scan(/\w+/)
       .take_while { |w| w !~ SEASON_EPISODE_REGEX }
       .map(&:capitalize)
       .join(' ')
       .gsub(' S ', "'s ")
+
+    show.empty? ? nil : show
   end
 
+  # Extract the season number from `@filename`.
+  # @return [Fixnum] if a match is found.
+  # @return [nil] if no match is found.
   def extract_season
     match = SEASON_EPISODE_REGEX.match(@filename)
     match && match[:season].to_i
   end
 
+  # Extract the episode number from `@filename`.
+  # @return [Fixnum] if a match is found.
+  # @return [nil] if no match is found.
   def extract_episode
     match = SEASON_EPISODE_REGEX.match(@filename)
     match && match[:episode].to_i
   end
 
+  # Extract the extension from `@filename`.
+  # @return [String] if a match is found.
+  # @return [nil] if no match is found.
   def extract_extension
     File.extname(@filename)[1..-1]
   end
