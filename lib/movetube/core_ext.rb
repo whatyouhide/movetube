52e11f1b41eaebab5bc8c0e1d51afdd1b9db3685
Andrea Leopardi <an.leopardi@gmail.com>
2014-09-14 17:27:21 +0200

    Remove all core extensions

--- lib/movetube/core_ext.rb
+++
@@ -1,8 +0,0 @@
-class Hash
-  # Select only `keys` keys from this hash.
-  # @param [Hash] keys
-  # @return [Hash]
-  def only_keys(*keys)
-    select { |key, value| keys.include?(key) }
-  end
-end
